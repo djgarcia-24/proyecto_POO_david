ed605c5ad046e91c74f2fedc3f29eb0c66fd0d42
davig <davig@ELMASGRANDE>
2025-11-03 18:11:33 -0600

    carga 1

---
+++ src/proyecto_david_octubre/Validaciones.java
@@ -0,0 +1,49 @@
+
+package proyecto_david_octubre;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Validaciones {
+    static Scanner scan = new Scanner(System.in);
+
+    
+    static public int validar_int(String mensaje, int max,int min){
+       int valor = 0 ;
+       
+       boolean repetir = true;
+       do {
+            try {
+                System.out.println(mensaje);
+                valor = scan.nextInt();
+                if (valor < min || valor >max) {
+                    System.out.println("No valido.");
+                } else {
+                    repetir = false;
+                }
+            } catch (InputMismatchException error) {
+                System.out.println("Error: Debes indicar un numero entero.");
+                scan.nextLine();
+            }
+        } while(repetir);
+
+      return valor;
+    }
+    
+    static public String validar_string( String mensaje, String regex){
+        String cadena;
+        boolean continuar = true;
+       
+       do {
+            System.out.print(mensaje);
+            cadena = scan.nextLine();
+            if (cadena.matches(regex)) {
+                continuar = false;
+             } 
+            else {
+                System.out.println("No valido.");
+            }
+       }  while(continuar);
+        return cadena;
+    }
+}
